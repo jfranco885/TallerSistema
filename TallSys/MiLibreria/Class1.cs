9152a2d216805f242100b9b626ba9ff6b7a6dc7b
jfranco885 <juancarlosfranco410@gmail.com>
2020-03-16 20:07:00 -0600

    Agregué clase DataSet en clase MiLibrería

---
+++ TallSys/MiLibreria/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MiLibreria
+{
+    public class Utilidades
+    {
+        public static DataSet Ejecutar(String consulta)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-G2V0SUK\\SQLEXPRESS;Initial Catalog=tallerdb;Integrated Security=True");
+
+            con.Open();
+            //  MessageBox.Show("Conexión con éxito");
+            DataSet dataSet = new DataSet();
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(consulta, con);
+
+            sqlDataAdapter.Fill(dataSet);//llenando el dataset
+            con.Close();//cerrando conexión
+
+            return dataSet;
+        }
+    }
+}
--- TallSys/TallSys/Form1.cs
+++ TallSys/TallSys/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace TallSys
 {
@@ -20,9 +21,12 @@ namespace TallSys
         
         private void btnIngresar_Click(object sender, EventArgs e)
         {
+           
+            
             MenuPrincipal menuPrincipal = new MenuPrincipal();
             this.Hide();
             menuPrincipal.Show();
+            
         }
     }
 }
