e441311aa6907b3af58a46a24ac1835dd60c19a4
jfranco885 <juancarlosfranco410@gmail.com>
2020-03-28 12:34:08 -0600

    insertar del mecanico casi terminado solo falta cambio de pieza

--- TallSys/TallSys/ServicioEnProceso.cs
+++ TallSys/TallSys/ServicioEnProceso.cs
@@ -7,19 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MiLibreria;
 
 namespace TallSys
 {
-    public partial class ServicioEnProceso : ContenedorVentanas
+    public partial class txtIdServicio : ContenedorVentanas
     {
-        public ServicioEnProceso()
+        public txtIdServicio()
         {
             InitializeComponent();
+            
+            edtCliente.Enabled = false;
+            edtEstadoActual.Enabled = false;
+            edtNave.Enabled = false;
+            edtTiempoEstablecido.Enabled = false;
+            edtTipoServicio.Enabled = false;
+            edtDescripcion.Enabled = false;
+
+            cboxEstadoNuevo.Text = edtEstadoActual.Text;
+           
         }
 
         private void ServicioEnProceso_Load(object sender, EventArgs e)
         {
-
+            cboxEstadoNuevo.DataSource = Utilidades.llenarComboBox("select idestado,estado from estado");
+            cboxEstadoNuevo.DisplayMember = "estado";
+            cboxEstadoNuevo.ValueMember = "idestado";
         }
 
         private void btnGestionar_Click(object sender, EventArgs e)
@@ -27,5 +40,49 @@ namespace TallSys
             MtoCambioPiezas mtoCambioPiezas = new MtoCambioPiezas();
             mtoCambioPiezas.Show();
         }
-    }
-}
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                String idDetalleServicio = txtServicio.Text;
+                String descripcion = edtDescripcion.Text.Trim();
+                String diagnostico=edtDiagnostico.Text.Trim();
+                int estado =int.Parse(cboxEstadoNuevo.SelectedValue.ToString());
+                String tiempoReal = edtTiempoReal.Text.Trim();
+
+                if (tiempoReal == "")
+                {
+                    tiempoReal = "0";
+                }
+                String actividad = edtActividad.Text.Trim();
+                int idnavetipo =int.Parse(edtIdTipoServicio.Text.Trim());
+                int idEncabezadoServicio =int.Parse(txtEncabezado.Text.Trim());
+            
+                String consulta = String.Format("EXEC insertarDatosElMecanico '{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}'", idDetalleServicio,descripcion,diagnostico,estado,tiempoReal,actividad,idnavetipo,idEncabezadoServicio);
+
+                Utilidades.Ejecutar(consulta);
+
+
+
+                //actualizando los datos en la vista
+
+                
+                edtTiempoReal.Text = tiempoReal;
+                edtEstadoActual.Text = cboxEstadoNuevo.Text;
+                
+                MessageBox.Show("Se han guardado los datos");
+
+              
+               
+               
+            }
+            catch (Exception errorGuardarC)
+            {
+                MessageBox.Show("Ha ocurrido un error" + errorGuardarC.Message);
+            }
+            
+        }
+    }//fin clase
+}//fin proyecto
