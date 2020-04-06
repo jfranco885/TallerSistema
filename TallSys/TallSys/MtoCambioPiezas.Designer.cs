21420256d00877f69d270940a0a8c62d6123497b
jfranco885 <juancarlosfranco410@gmail.com>
2020-04-06 10:17:17 -0600

    Mto cambio piezas terminado

--- TallSys/TallSys/MtoCambioPiezas.Designer.cs
+++ TallSys/TallSys/MtoCambioPiezas.Designer.cs
@@ -29,46 +29,58 @@
         private void InitializeComponent()
         {
             this.label2 = new System.Windows.Forms.Label();
-            this.edtid = new System.Windows.Forms.TextBox();
+            this.edtidCambio = new System.Windows.Forms.TextBox();
             this.edtRepuesto = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.edtServicio = new System.Windows.Forms.TextBox();
+            this.edtIdServicio = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.edtCantidad = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(247, 79);
+            this.label1.Location = new System.Drawing.Point(194, 79);
             // 
             // btnBuscar
             // 
             this.btnBuscar.FlatAppearance.BorderSize = 0;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click_1);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click_1);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click_1);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(269, 141);
+            this.label2.Location = new System.Drawing.Point(262, 148);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(78, 13);
             this.label2.TabIndex = 16;
             this.label2.Text = "Código Cambio";
             // 
-            // edtid
+            // edtidCambio
             // 
-            this.edtid.Location = new System.Drawing.Point(375, 138);
-            this.edtid.Name = "edtid";
-            this.edtid.Size = new System.Drawing.Size(270, 20);
-            this.edtid.TabIndex = 17;
+            this.edtidCambio.Location = new System.Drawing.Point(368, 145);
+            this.edtidCambio.Name = "edtidCambio";
+            this.edtidCambio.Size = new System.Drawing.Size(270, 20);
+            this.edtidCambio.TabIndex = 17;
             // 
             // edtRepuesto
             // 
-            this.edtRepuesto.Location = new System.Drawing.Point(375, 190);
+            this.edtRepuesto.Location = new System.Drawing.Point(368, 197);
             this.edtRepuesto.Name = "edtRepuesto";
             this.edtRepuesto.Size = new System.Drawing.Size(270, 20);
             this.edtRepuesto.TabIndex = 19;
+            this.edtRepuesto.TextChanged += new System.EventHandler(this.edtRepuesto_TextChanged);
             // 
             // label3
             // 
@@ -82,7 +94,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(269, 193);
+            this.label4.Location = new System.Drawing.Point(262, 200);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(93, 13);
             this.label4.TabIndex = 20;
@@ -91,30 +103,50 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(293, 79);
+            this.label5.Location = new System.Drawing.Point(240, 79);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.Size = new System.Drawing.Size(100, 13);
             this.label5.TabIndex = 22;
-            this.label5.Text = "Id Servicio";
+            this.label5.Text = "por id Cambio Pieza";
             // 
-            // edtServicio
+            // edtIdServicio
             // 
-            this.edtServicio.Location = new System.Drawing.Point(36, 116);
-            this.edtServicio.Name = "edtServicio";
-            this.edtServicio.Size = new System.Drawing.Size(161, 20);
-            this.edtServicio.TabIndex = 23;
+            this.edtIdServicio.Location = new System.Drawing.Point(36, 116);
+            this.edtIdServicio.Name = "edtIdServicio";
+            this.edtIdServicio.Size = new System.Drawing.Size(161, 20);
+            this.edtIdServicio.TabIndex = 23;
+            this.edtIdServicio.TextChanged += new System.EventHandler(this.edtIdServicio_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(262, 252);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 13);
+            this.label6.TabIndex = 25;
+            this.label6.Text = "Cantidad";
+            // 
+            // edtCantidad
+            // 
+            this.edtCantidad.Location = new System.Drawing.Point(368, 249);
+            this.edtCantidad.Name = "edtCantidad";
+            this.edtCantidad.Size = new System.Drawing.Size(270, 20);
+            this.edtCantidad.TabIndex = 24;
+            this.edtCantidad.TextChanged += new System.EventHandler(this.edtCantidad_TextChanged);
             // 
             // MtoCambioPiezas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(947, 474);
-            this.Controls.Add(this.edtServicio);
+            this.ClientSize = new System.Drawing.Size(947, 488);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.edtCantidad);
+            this.Controls.Add(this.edtIdServicio);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.edtRepuesto);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.edtid);
+            this.Controls.Add(this.edtidCambio);
             this.Controls.Add(this.label2);
             this.Name = "MtoCambioPiezas";
             this.Text = "MtoCambioPiezas";
@@ -128,12 +160,14 @@
             this.Controls.SetChildIndex(this.label1, 0);
             this.Controls.SetChildIndex(this.edtBuscar, 0);
             this.Controls.SetChildIndex(this.label2, 0);
-            this.Controls.SetChildIndex(this.edtid, 0);
+            this.Controls.SetChildIndex(this.edtidCambio, 0);
             this.Controls.SetChildIndex(this.label3, 0);
             this.Controls.SetChildIndex(this.edtRepuesto, 0);
             this.Controls.SetChildIndex(this.label4, 0);
             this.Controls.SetChildIndex(this.label5, 0);
-            this.Controls.SetChildIndex(this.edtServicio, 0);
+            this.Controls.SetChildIndex(this.edtIdServicio, 0);
+            this.Controls.SetChildIndex(this.edtCantidad, 0);
+            this.Controls.SetChildIndex(this.label6, 0);
             this.groupBox1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
@@ -144,11 +178,13 @@
         #endregion
 
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox edtid;
+        private System.Windows.Forms.TextBox edtidCambio;
         private System.Windows.Forms.TextBox edtRepuesto;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox edtServicio;
+        private System.Windows.Forms.TextBox edtIdServicio;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox edtCantidad;
     }
 }
--- TallSys/TallSys/MtoCambioPiezas.cs
+++ TallSys/TallSys/MtoCambioPiezas.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MiLibreria;
 
 namespace TallSys
 {
@@ -15,11 +16,205 @@ namespace TallSys
         public MtoCambioPiezas()
         {
             InitializeComponent();
+            cargarTabla();
+            desactivarControles();
+            edtidCambio.Enabled = false;
         }
 
+        public override void guardar()
+        {
+            if (validarCampo(edtIdServicio)| validarCampo(edtRepuesto) | validarCampo(edtCantidad))
+            {
+                //solo mandara el error provider si está vacío
+            }
+            else
+            {
+                Boolean existe;
+                String idServicio = edtIdServicio.Text.Trim();
+                String consul = String.Format("select *from detalle_servicios where iddetalle_servicio='{0}'", idServicio);
+                existe = Utilidades.Existe(consul);
+
+                if (existe == true)
+                {
+                    //Hacer la inserción
+                    try
+                    {
+                        String consulta = String.Format("EXEC insertarCambioPieza '{0}','{1}','{2}'", edtRepuesto.Text.Trim(), edtIdServicio.Text.Trim(), Convert.ToInt16(edtCantidad.Text.Trim()));
+                        Utilidades.Ejecutar(consulta);
+                        MessageBox.Show("Se han guardado los datos");
+                        limpiarCampos();
+                        cargarTabla();
+                        desactivarControles();
+                        btnGuardar.Enabled = false;
+                    }
+                    catch (Exception er)
+                    {
+                        MessageBox.Show("Ha ocurrido un error" + er.Message);
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("No existe este id de Servicio");
+                }
+            }
+        }
+
+        public override void eliminar()
+        {
+            DialogResult resul = MessageBox.Show("Seguro que quiere eliminar el Registro?", "Eliminar Registro", MessageBoxButtons.YesNo);
+            if (resul == DialogResult.Yes)
+            {
+                try
+                {
+                    Utilidades.eliminarRegistro("CambioPieza", edtidCambio.Text);//envio el complemento del nombre del proc almacenado y el id del que quiero eliminar
+                    limpiarCampos();
+                    desactivarControles();
+                    cargarTabla();
+                    btnEditar.Enabled = false;
+                    btnEliminar.Enabled = false;
+
+                }
+                catch (Exception errr)
+                {
+                    MessageBox.Show("Ha ocurrido un error" + errr.Message);
+                }
+            }
+
+        }
+        private void btnBuscar_Click_1(object sender, EventArgs e)
+        {
+            if (validarCampo(edtBuscar))
+            {
+                //solo mandara el error provider si está vacío
+            }
+            else
+            {
+                try
+                {
+                    String edtbuscar = edtBuscar.Text.Trim();
+                    DataSet dt;
+                    String consul = String.Format("select *from cambio_piezas where idcambio_pieza='{0}'", edtbuscar);
+                    dt = Utilidades.Ejecutar(consul);
+                    if (dt.Tables[0].Rows.Count > 0)
+                    {
+                        edtIdServicio.Text = (dt.Tables[0].Rows[0]["iddetalle_servicio"].ToString().Trim());
+                        edtidCambio.Text = (dt.Tables[0].Rows[0]["idcambio_pieza"].ToString().Trim());
+                        edtRepuesto.Text = (dt.Tables[0].Rows[0]["nombre_pieza"].ToString().Trim());
+                        edtCantidad.Text = (dt.Tables[0].Rows[0]["cantidad"].ToString().Trim());
+
+                        activarControles();
+                        btnEditar.Enabled = true;
+                        btnEliminar.Enabled = true;
+                        btnGuardar.Enabled = false;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Este cargo no existe");
+                        limpiarCampos();
+                       
+
+                    }
+                }
+                catch (Exception errr)
+                {
+                    MessageBox.Show("Ha ocurrido un error" + errr.Message);
+                }
+
+            }
+        }
+        private void btnEditar_Click_1(object sender, EventArgs e)
+        {
+            if (validarCampo(edtIdServicio) | validarCampo(edtRepuesto) | validarCampo(edtCantidad))
+            {
+                //solo mandara el error provider si está vacío
+            }
+            else
+            {
+                Boolean existe;
+                String idServicio = edtIdServicio.Text.Trim();
+                String consul = String.Format("select *from detalle_servicios where iddetalle_servicio='{0}'", idServicio);
+                existe = Utilidades.Existe(consul);
+
+                if (existe == true)
+                {
+                    //Hacer la inserción
+                    try
+                    {
+                        String consulta = String.Format("EXEC actualizarCambioPieza '{0}','{1}','{2}','{3}'",edtidCambio.Text, edtRepuesto.Text.Trim(), edtIdServicio.Text.Trim(), Convert.ToInt16(edtCantidad.Text.Trim()));
+                        Utilidades.Ejecutar(consulta);
+                        MessageBox.Show("Se han actualizado los datos");
+                        limpiarCampos();
+                        cargarTabla();
+                        desactivarControles();
+                        btnEliminar.Enabled = false;
+                        btnEditar.Enabled = false;
+                    }
+                    catch (Exception er)
+                    {
+                        MessageBox.Show("Ha ocurrido un error" + er.Message);
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("No existe este id de Servicio");
+                }
+            }
+        }
+        private void btnNuevo_Click_1(object sender, EventArgs e)
+        {
+            limpiarCampos();
+            activarControles();
+            edtIdServicio.Focus();
+        }
         private void MtoCambioPiezas_Load(object sender, EventArgs e)
         {
 
         }
-    }
-}
+        private void cargarTabla()
+        {
+            tabla.DataSource = Utilidades.datasetLista("listarCambioPieza").Tables[0];
+        }
+
+        private void activarControles()
+        {
+            edtIdServicio.Enabled = true;
+            edtRepuesto.Enabled = true;
+            edtCantidad.Enabled = true;
+        }
+        private void desactivarControles()
+        {
+                edtIdServicio.Enabled = false;
+                edtRepuesto.Enabled = false;
+                edtCantidad.Enabled = false;
+            
+        }
+
+        private void limpiarCampos()
+        {
+
+            edtIdServicio.Text = "";
+            edtRepuesto.Text = "";
+            edtCantidad.Text = "";
+            edtBuscar.Text = "";
+            edtidCambio.Text = "";
+
+        }
+
+        private void edtRepuesto_TextChanged(object sender, EventArgs e)
+        {
+            errorProvider1.Clear();
+        }
+
+        private void edtCantidad_TextChanged(object sender, EventArgs e)
+        {
+            errorProvider1.Clear();
+        }
+
+        private void edtIdServicio_TextChanged(object sender, EventArgs e)
+        {
+            errorProvider1.Clear();
+        }
+    }//fin clase
+}//fin proyecto
